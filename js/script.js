318853e7b88d57d894a9b011db205a9f4de49f9c
jultsvirbut <jul.tsvirbut@gmail.com>
2015-12-08 02:00:31 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+function initialize () {
+    var mapCanvas = document.getElementById('map');
+    var myLatLng = {lat: 53.893900, lng: 27.551298};
+    var mapOptions = {
+      center: new google.maps.LatLng(53.893900, 27.551298),
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'DODO - магазин-студия декора и подарков'
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
